de01c01396d79fdc19b63230438c21059fac7074
yanad <yana.danilova1509@gmail.com>
2023-08-09 10:24:21 +0200

    swagger

--- src/main/java/com/springboot/webfluxapp/controllers/TaskScheduleController.java
+++ src/main/java/com/springboot/webfluxapp/controllers/TaskScheduleController.java
@@ -1,6 +1,7 @@
 package com.springboot.webfluxapp.controllers;
 import com.springboot.webfluxapp.entities.TaskSchedule;
 import com.springboot.webfluxapp.services.TaskScheduleService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,25 +21,29 @@ public class TaskScheduleController {
     }
 
     @GetMapping()
+    @Operation(description = "get the entire list of task schedule")
     public Flux<TaskSchedule> getAllTaskSchedule(){
         return taskScheduleService.getAllTaskSchedule();
     }
 
     @GetMapping("/{id}")
+    @Operation(description = "get a task schedule by ID")
     public Mono<ResponseEntity<TaskSchedule>> getTaskScheduleById(@PathVariable int id){
         return taskScheduleService.getTaskSchedule(id).map(TaskSchedule->ResponseEntity.ok().body(TaskSchedule))
                 .switchIfEmpty(Mono.just(ResponseEntity.notFound().build()));
     }
     @PostMapping
+    @Operation(description = "Create a new Task Schedule")
     public Mono<TaskSchedule> createTaskSchedule(@RequestBody TaskSchedule taskSchedule){
        return taskScheduleService.create(taskSchedule);
     }
     @PutMapping("/{id}")
-
+    @Operation(description = "Update the existing task schedule")
     public Mono<TaskSchedule> updateTaskSchedule(@RequestBody TaskSchedule updatedTaskSchedule, @PathVariable int id){
       return taskScheduleService.update(updatedTaskSchedule,id);
     }
     @DeleteMapping("/{id}")
+    @Operation(description = "Delete the existing task schedule by ID")
     public Mono<ResponseEntity<Object>> deleteTaskScheduleById(@PathVariable int id){
         return taskScheduleService.deleteById(id)
                 .then(Mono.just(ResponseEntity.noContent().build()))
